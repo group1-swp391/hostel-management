aafeca1355c9ed7d8369f7e3cffd3aac8f01d476
Long56642002 <longhoangdinh212@gmail.com>
2022-06-03 14:03:34 +0700

    crud room

--- src/main/java/com/example/hostelmanagement/controllers/RoomController.java
+++ src/main/java/com/example/hostelmanagement/controllers/RoomController.java
@@ -4,14 +4,16 @@ import com.example.hostelmanagement.entities.Room;
 import com.example.hostelmanagement.entities.User;
 import com.example.hostelmanagement.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "api/v1/Room/")
@@ -24,47 +26,63 @@ public class RoomController {
         return "host_roomMngt";
     }
 
-//    @PostMapping(value = "insert")
-//    public String insertRoom(ModelMap mm, HttpSession session, @RequestParam("newRoomNumber") int newRoomNumber, @RequestParam("newUserID") int newUserID, @RequestParam("newTypeID") int newTypeID){
-//        User loginUser = (User) session.getAttribute("LOGIN_USER");
-//        roomRepository.save(new Room(newRoomNumber, loginUser.getUserId(), newTypeID, true));
-//        mm.put("message", "Insert new hostel successfully!");
-//        return "host_roomMngt";
-//    }
-//
-//    @GetMapping(value = "delete")
-//    public String deleteRoom(ModelMap mm, @RequestParam(value = "RoomId") int roomId) {
-//        try {
-//            Room room = roomRepository.findById(roomId).get();
-//            room.setRoomStatus(false);
-//            roomRepository.save(room);
-//            mm.put("message","Delete room successfully");
-//        } catch (Exception e) {
-//            mm.put("message", "Delete room failed");
-//        } finally {
-//            return "host_roomMngt";
-//        }
-//    }
-//    @GetMapping(value = "update")
-//    public String updateRoom(ModelMap mm, @RequestParam(value = "hostelID") int hostelID, @RequestParam("hostelName") String hostelName, @RequestParam("address") String address, @RequestParam("room_quality") int room_quality) {
-//        try {
-//            Hostel hostel = roomRepository.findById(hostelID).get();
-//            hostel.setHostelName(hostelName);
-//            hostel.setAddress(address);
-//            hostel.setRoom_quality(room_quality);
-//            roomRepository.save(hostel);
-//            mm.put("message","Update hostel successfully");
-//        } catch (Exception e) {
-//            mm.put("message", "Update hostel failed");
-//        } finally {
-//            return "host_roomMngt";
-//        }
-//    }
+    private byte[] getByteImage(Part image) throws IOException {
+        return image.getInputStream().readAllBytes();
+    }
+
+    @PostMapping(value = "insert")
+    public String insertRoom(ModelMap mm, HttpSession session, @RequestParam("newRoomNumber") int newRoomNumber, @RequestParam("newTypeId") int newTypeId, @RequestParam("hostelId") int hostelId, @RequestParam("newImage") Part newImage) throws IOException {
+        User loginUser = (User) session.getAttribute("LOGIN_USER");
+        roomRepository.save(new Room(newRoomNumber, loginUser.getUserId(), newTypeId, true, getByteImage(newImage), hostelId));
+        mm.put("message", "Insert new hostel successfully!");
+        return "host_roomMngt";
+    }
+
+    @PostMapping(value = "delete")
+    public String deleteRoom(ModelMap mm, @RequestParam(value = "roomId") int roomId) {
+        try {
+            Room room = roomRepository.findById(roomId).get();
+            room.setRoomStatus(false);
+            roomRepository.save(room);
+            mm.put("message","Delete room successfully");
+        } catch (Exception e) {
+            mm.put("message", "Delete room failed");
+        } finally {
+            return "host_roomMngt";
+        }
+    }
+
+    @PostMapping(value = "update")
+    public String updateRoom(ModelMap mm, @RequestParam(value = "roomId") int roomId, @RequestParam("roomNumber") int roomNumber, @RequestParam("typeId") int typeId, @RequestParam("image") Part image) {
+        try {
+            Room room = roomRepository.findById(roomId).get();
+            room.setRoomNumber(roomNumber);
+            room.setTypeId(typeId);
+            room.setImage(getByteImage(image));
+            roomRepository.save(room);
+            mm.put("message","Update room successfully");
+        } catch (Exception e) {
+            mm.put("message", "Update room failed");
+        } finally {
+            return "host_roomMngt";
+        }
+    }
 
     @GetMapping(value = "search")
-    public String getAllRooms(@RequestParam("roomNumber") int roomNumber, HttpSession session, ModelMap mm) {
+    public String getAllRooms(@RequestParam("roomNumber") int roomNumber, ModelMap mm) {
         mm.put("roomNumber", roomNumber);
-        mm.put("hostels", roomRepository.findAllByRoomNumber(roomNumber));
+        mm.put("rooms", roomRepository.findAllByRoomNumber(roomNumber));
         return "host_roomMngt";
+    }
+    @ResponseBody
+    @GetMapping("image/{id}")
+    public ResponseEntity<byte[]> fromDatabaseAsResEntity(@PathVariable("id") int id) {
+            Optional<Room> room = roomRepository.findById(id);
+            byte[] imageBytes = null;
+            if (room.isPresent()) {
+
+                imageBytes = room.get().getImage();
+            }
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
     }
 }
--- src/main/java/com/example/hostelmanagement/repositories/RoomRepository.java
+++ src/main/java/com/example/hostelmanagement/repositories/RoomRepository.java
@@ -2,11 +2,14 @@ package com.example.hostelmanagement.repositories;
 
 import com.example.hostelmanagement.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
-    List<Room> findAllByRoomNumber(int roomNumber);
+    @Query(nativeQuery = true, value = "SELECT * FROM tbl_Room r WHERE r.roomNumber = :roomNumber AND r.roomStatus=1")
+    List<Room> findAllByRoomNumber(@Param("roomNumber") int roomNumber);
 }
