83fc5f1fd33dcd8e90a7a5c5a3fbbead0687a62d
Long56642002 <longhoangdinh212@gmail.com>
2022-06-05 00:36:45 +0700

    fix room function

--- src/main/java/com/example/hostelmanagement/repositories/RoomRepository.java
+++ src/main/java/com/example/hostelmanagement/repositories/RoomRepository.java
@@ -2,7 +2,6 @@ package com.example.hostelmanagement.repositories;
 
 import com.example.hostelmanagement.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
