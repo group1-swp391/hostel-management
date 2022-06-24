fb765cce3ca35204de9a3761951b4028877bbbef
Long56642002 <longhoangdinh212@gmail.com>
2022-06-24 09:53:13 +0700

    add packaging war

--- src/main/java/com/example/hostelmanagement/HostelManagementApplication.java
+++ src/main/java/com/example/hostelmanagement/HostelManagementApplication.java
@@ -3,16 +3,18 @@ package com.example.hostelmanagement;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HostelManagementApplication implements CommandLineRunner {
+public class HostelManagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HostelManagementApplication.class, args);
     }
 
-
     @Override
-    public void run(String... args) throws Exception {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HostelManagementApplication.class);
     }
 }
